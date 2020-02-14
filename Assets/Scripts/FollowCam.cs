739a701e5fab7fad891a7397fd53ef22e7e3fb80
AlecVidamo <55145798+AlecVidamo@users.noreply.github.com>
2020-02-13 19:00:38 -0500

    Created wall prefab and material

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -21,7 +21,8 @@ public class FollowCam : MonoBehaviour
         if (POI == null) return;
         Vector3 destination = POI.transform.position;
         destination.x = Mathf.Max(minXY.x, destination.x);
-        destination.y = Mathf.Max(minXY.y, destination.y);        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
         Camera.main.orthographicSize = destination.y + 10;
